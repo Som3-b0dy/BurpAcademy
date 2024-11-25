ff850c9fd29368d1f44248270e7c8e24ddc148d8
s0m3body <pisdushko1886@gmail.com>
2024-11-25 22:42:19 +0300

    Changes: 1) Created a crawler to automatically fetch unique endpoints from HTML 2) Testing for basic URL SQLi in those endpoints

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+
+from SQLi.url_param_sqli import *
+
+def main():
+   try:
+      url = sys.argv[1]
+      try_url_param_sqli(url)
+   except IndexError:
+      print("[!] url param is required")
+      print(f"[*] python3 {sys.argv[0]} <url>")
+
+if __name__ == "__main__":
+   main()
